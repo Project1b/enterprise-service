0659fd9860ec6e16b130a47a371e152b15c68f80
Erick Pinglo <erick.pinglo@hotmail.com>
2022-05-10 12:47:10 -0500

    first commit

---
+++ src/main/java/pe/com/bank/enterprise/client/PersonEnterpriseRestClient.java
@@ -0,0 +1,38 @@
+package pe.com.bank.enterprise.client;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import pe.com.bank.enterprise.entity.PersonEnterprise;
+import reactor.core.publisher.Mono;
+
+public class PersonEnterpriseRestClient {
+	
+	
+	 private WebClient webClient;		
+	  
+	  public PersonEnterpriseRestClient(WebClient webClient) {
+	        this.webClient = webClient;
+	    }
+	  
+	  
+	  @Value("${restClient.personEnterprisesUrl}")
+	  private String personEnterpriseUrl;
+	  
+	  public Mono<PersonEnterprise> getPersonEnterprise(String enterpriseId){
+		  
+		  var url = UriComponentsBuilder.fromHttpUrl(personEnterpriseUrl)
+	                .queryParam("enterpriseId", enterpriseId)
+	                .buildAndExpand().toUriString();
+		  
+		  return  webClient
+	                .get()
+	                .uri(url)
+	                .retrieve()
+	                .bodyToMono(PersonEnterprise.class)
+	                .log();
+ 
+	  }  
+
+}
