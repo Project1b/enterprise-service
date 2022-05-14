e503eff7221fa517bba34be842b473c815be32a2
Erick Pinglo <erick.pinglo@hotmail.com>
2022-05-13 20:51:30 -0500

    update

--- src/main/java/pe/com/bank/enterprise/client/PersonEnterpriseRestClient.java
+++ src/main/java/pe/com/bank/enterprise/client/PersonEnterpriseRestClient.java
@@ -1,12 +1,14 @@
 package pe.com.bank.enterprise.client;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import pe.com.bank.enterprise.entity.PersonEnterprise;
 import reactor.core.publisher.Mono;
 
+@Component
 public class PersonEnterpriseRestClient {
 	
 	
